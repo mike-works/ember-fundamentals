9c007a1397b22898e825625006649c3a1f8a31a6
Mike North <michael.l.north@gmail.com>
2017-03-13 23:42:38 -0700

    Exercise 7

--- tests/acceptance/exercise7-test.js
+++ tests/acceptance/exercise7-test.js
@@ -1,29 +1,30 @@
 import { test } from 'qunit';
 import moduleForAcceptance from 'emberli/tests/helpers/module-for-acceptance';
 
-if (require.entries['emberli/routes/course'] && !require.entries['ember-network/fetch']) {
-  moduleForAcceptance('Exercise 6 | Basic Routing Structure');
+if (require.entries['emberli/routes/course'] && require.entries['ember-network/fetch']) {
+  moduleForAcceptance('Exercise 7 | Async Data');
 
   test('visiting /', function(assert) {
     visit('/');
 
     andThen(() => {
       assert.equal(currentURL(), '/');
-      let $phoenixLink = find('ul a[href="/course/Phoenix%20Fundamentals"]');
-      assert.equal($phoenixLink.length, 1, 'Link with href="/course/Phoenix%20Fundamentals" is present on the page');
+      let $phoenixLink = find('ul a[href="/course/phoenix-fundamentals-f8c47eb"]');
+      assert.equal($phoenixLink.length, 1, 'Link with href="/course/phoenix-fundamentals-f8c47eb" is present on the page');
       assert.ok($phoenixLink.text().trim().indexOf('Phoenix Fundamentals') >= 0, 'Phoenix Fundamentals is present in a link on the "/" page');
 
-      let $emberBasicsLink = find('ul a[href="/course/Ember%20Basics"]');
-      assert.equal($emberBasicsLink.length, 1, 'Link with href="/course/Ember%20Basics" is present on the page');
+      let $emberBasicsLink = find('ul a[href="/course/ember-basics-cf22ed3"]');
+      assert.equal($emberBasicsLink.length, 1, 'Link with href="/course/ember-basics-cf22ed3" is present on the page');
       assert.ok($emberBasicsLink.text().trim().indexOf('Ember Basics') >= 0, 'Ember Basics is present in a link on the "/" page');
     });
 
-    click('ul a[href="/course/Ember%20Basics"]');
+    click('ul a[href="/course/ember-basics-cf22ed3"]');
 
     andThen(() => {
       assert.ok(find('h1').length > 0, 'At least one H1 on the page');
       assert.ok(find('h1.course-title').length > 0, 'H1 has a class .course-title');
       assert.equal(find('h1.course-title').text(), 'Ember Basics', 'Course title is rendered inside the h1.course-title');
     });
+    
   });
 }
