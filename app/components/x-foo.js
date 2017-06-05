5247f00f53e7365fb2b119d4152a858cb0d23f77
Michael North <michael.l.north@gmail.com>
2017-06-05 15:18:55 -0400

    color picker

--- app/components/x-foo.js
+++ app/components/x-foo.js
@@ -1,7 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    tagName: 'span',
-    attributeBindings: ['username:data-twitter-username']
+    r: 100,
+    g: 100,
+    b: 200,
+    hex: Ember.computed('r', 'g', 'b', {
+        get() {
+            let { r, g, b} = this.getProperties('r', 'g', 'b');
+            return [r, g, b]
+                .map((ch) => parseInt(ch, 10).toString(16))
+                .map((ch) => ch.length < 2 ? `0${ch}` : ch)
+                .join('');
+        },
+        set(_, newVal) {
+            let [r, g, b] = [0, 2, 4]
+                .map((offset) => newVal.substring(offset, offset+2))
+                .map((hexCh) => parseInt(hexCh, 16));
+            this.setProperties({
+                r, g, b
+            });
+            return newVal;
+        }
+    }),
+    _boxStyle: Ember.computed('r', 'g', 'b', function() {
+        let { r, g, b} = this.getProperties('r', 'g', 'b');
+        let x = 'width: 100px; height: 100px;';
+        return `${x}background: rgb(${r}, ${g}, ${b});`.htmlSafe();
+    })
 });
 
