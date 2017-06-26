27787448333b5b81298a1eeb2e3f98b486be8148
Michael North <michael.l.north@gmail.com>
2017-06-26 15:31:26 -0700

    fetching data from an API

--- app/routes/course.js
+++ app/routes/course.js
@@ -2,9 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model(params) {
-        let courses = this.modelFor('application');
-        return courses.findBy('title', params.id);
-        // return courses
-        //     .filter((c) => c.title === params.id)[0];
+        // let courses = this.modelFor('application');
+        // return courses.findBy('slug', params.id);
+        return fetch(`https://api.mike.works/api/v1/courses/${params.id}`)
+            .then((resp) => resp.json())
+            .then((jsonData) => jsonData.data.attributes);
+
     }
 }); 
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,13 @@
 import Ember from 'ember';
+import fetch from 'ember-network/fetch';
 
 export default Ember.Route.extend({
     model() {
-        return this.modelFor('application');
+       return fetch('https://api.mike.works/api/v1/courses')
+      .then((resp) => resp.json())
+      .then((jsonData) => {
+        return jsonData.data
+          .map((item) => item.attributes);
+      });
     }
 });
