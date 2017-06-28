5232791025c277c84722ba080c432e28ceabcc9e
Michael North <michael.l.north@gmail.com>
2017-06-28 15:58:28 -0700

    Fetch

--- app/routes/course.js
+++ app/routes/course.js
@@ -2,7 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model({ id }) {
-        return this.modelFor('application')
-            .filter((course) => course.title === id)[0]
+        return this
+            .modelFor('application')
+            .filter((course) => course.slug === id)[0]
     }
 });
