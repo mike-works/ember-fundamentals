a8f15c8e2badbc6674cdeb6d865dc29bf2a28cc5
Michael North <michael.l.north@gmail.com>
2017-10-03 11:56:31 +0530

    Exercise 1: data model

--- app/utils/course.js
+++ app/utils/course.js
@@ -1,6 +1,21 @@
 import Ember from 'ember';
 
 const Course = Ember.Object.extend({
-  
+  title: 'No Title',
+  description: '',
+  // tags: [], < initialized per instance
+  // languages: [],  < initialized per instance
+  init() {
+    this.set('tags', []);
+    this.set('languages', []);
+  }
 });
+
+Course.reopenClass({
+  _languages: {js: 'JavaScript'},
+  languageName(abbrev) {
+    return Course._languages[abbrev] || 'Unknown';
+  }
+});
+
 export default Course;
