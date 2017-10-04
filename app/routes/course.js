083c9647669c66c5b328c6b0414de9be838dc909
Michael North <michael.l.north@gmail.com>
2017-10-04 15:27:40 +0530

    fetch

--- app/routes/course.js
+++ app/routes/course.js
@@ -4,6 +4,6 @@ export default Ember.Route.extend({
   model(params) {
     let id = params.id; 
     let courses = this.modelFor('application');
-    return courses.filter(item => item.title === id)[0];
+    return courses.filter(item => item.slug === id)[0];
   }
 });
