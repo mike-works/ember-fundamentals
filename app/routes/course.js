080cd5fa7a45cfe102bbb8157f5acd69ec0e70d3
Michael North <michael.l.north@gmail.com>
2017-10-19 16:48:39 +0800

    exercise 6

--- app/routes/course.js
+++ app/routes/course.js
@@ -1,4 +1,14 @@
 import Route from '@ember/routing/route';
+import allCourses from '../course-list';
 
 export default Route.extend({
+  model(params) {
+    let found = allCourses.filter(c => c.title === params.name);
+    switch (found.length) {
+      case 0: throw new Error('No course found!');
+      case 1: break;
+      default: throw new Error('Too many courses found!');
+    }
+    return found[0];
+  }
 });
