3a16d6d6ecf2fa6f5207746db4f85c068dd665ce
Michael North <michael.l.north@gmail.com>
2017-12-17 20:46:13 -0800

    Prettier

--- tests/acceptance/exercise-6-test.js
+++ tests/acceptance/exercise-6-test.js
@@ -2,7 +2,11 @@ import { test } from 'qunit';
 import moduleForAcceptance from 'emberli/tests/helpers/module-for-acceptance';
 import require from 'require';
 
-if (require.has('emberli/routes/course') && !require.has('ember-network/fetch') && !require.has('fetch')) {
+if (
+  require.has('emberli/routes/course') &&
+  !require.has('ember-network/fetch') &&
+  !require.has('fetch')
+) {
   moduleForAcceptance('Exercise 6 | Basic Routing Structure');
 
   test('visiting /', function(assert) {
@@ -11,20 +15,47 @@ if (require.has('emberli/routes/course') && !require.has('ember-network/fetch') 
     andThen(() => {
       assert.equal(currentURL(), '/');
       let $phoenixLink = find('ul a[href="/course/Phoenix%20Fundamentals"]');
-      assert.equal($phoenixLink.length, 1, 'Link with href="/course/Phoenix%20Fundamentals" is present on the page');
-      assert.ok($phoenixLink.text().trim().indexOf('Phoenix Fundamentals') >= 0, 'Phoenix Fundamentals is present in a link on the "/" page');
+      assert.equal(
+        $phoenixLink.length,
+        1,
+        'Link with href="/course/Phoenix%20Fundamentals" is present on the page'
+      );
+      assert.ok(
+        $phoenixLink
+          .text()
+          .trim()
+          .indexOf('Phoenix Fundamentals') >= 0,
+        'Phoenix Fundamentals is present in a link on the "/" page'
+      );
 
       let $emberBasicsLink = find('ul a[href="/course/Ember%20Basics"]');
-      assert.equal($emberBasicsLink.length, 1, 'Link with href="/course/Ember%20Basics" is present on the page');
-      assert.ok($emberBasicsLink.text().trim().indexOf('Ember Basics') >= 0, 'Ember Basics is present in a link on the "/" page');
+      assert.equal(
+        $emberBasicsLink.length,
+        1,
+        'Link with href="/course/Ember%20Basics" is present on the page'
+      );
+      assert.ok(
+        $emberBasicsLink
+          .text()
+          .trim()
+          .indexOf('Ember Basics') >= 0,
+        'Ember Basics is present in a link on the "/" page'
+      );
     });
 
     click('ul a[href="/course/Ember%20Basics"]');
 
     andThen(() => {
       assert.ok(find('h1').length > 0, 'At least one H1 on the page');
-      assert.ok(find('h1.course-title').length > 0, 'H1 has a class .course-title');
-      assert.equal(find('h1.course-title').text(), 'Ember Basics', 'Course title is rendered inside the h1.course-title');
+      assert.ok(
+        find('h1.course-title').length > 0,
+        'H1 has a class .course-title'
+      );
+      assert.equal(
+        find('h1.course-title').text(),
+        'Ember Basics',
+        'Course title is rendered inside the h1.course-title'
+      );
     });
   });
 }
--- tests/integration/components/course-list-item-test.js
+++ tests/integration/components/course-list-item-test.js
@@ -3,13 +3,22 @@ import hbs from 'htmlbars-inline-precompile';
 import require from 'require';
 
 if (require.has('emberli/components/course-list-item')) {
-  moduleForComponent('course-list-item', 'Exercise 3 | course-list-item component', {
-    integration: true
-  });
+  moduleForComponent(
+    'course-list-item',
+    'Exercise 3 | course-list-item component',
+    {
+      integration: true
+    }
+  );
 
   test('it renders', function(assert) {
     this.render(hbs`{{course-list-item}}`);
-    assert.equal(this.$().text().trim(), '');
+    assert.equal(
+      this.$()
+        .text()
+        .trim(),
+      ''
+    );
   });
 
   test('DOM structure is as expected', function(assert) {
@@ -21,15 +30,41 @@ if (require.has('emberli/components/course-list-item')) {
           url: 'abc'
         }
       }
-    })
+    });
     this.render(hbs`{{course-list-item model=model}}`);
     let $components = this.$().children('.course-list-item');
-    assert.equal($components.length, 1, 'Component has top-level class of .course-list-item');    
+    assert.equal(
+      $components.length,
+      1,
+      'Component has top-level class of .course-list-item'
+    );
     let prop = $components[0].attributes.getNamedItem('data-name');
-    assert.equal(prop ? prop.value : null, 'My Course', 'Course title can be found in component\'s boundary element, under DOM attribute "data-name"');
-    assert.equal($components.find('.course-title').text().trim(), 'My Course', 'Course title can be found in a .course-title element, within the component');
-    assert.equal($components.find('.course-summary').text().trim(), 'A course description', 'Course summary can be found in a .course-summary element, within the component');
-    assert.equal($components.find('img.course-image').attr('src'), 'abc', 'Course image can be found in a .course-image img element, within the component');
+    assert.equal(
+      prop ? prop.value : null,
+      'My Course',
+      'Course title can be found in component\'s boundary element, under DOM attribute "data-name"'
+    );
+    assert.equal(
+      $components
+        .find('.course-title')
+        .text()
+        .trim(),
+      'My Course',
+      'Course title can be found in a .course-title element, within the component'
+    );
+    assert.equal(
+      $components
+        .find('.course-summary')
+        .text()
+        .trim(),
+      'A course description',
+      'Course summary can be found in a .course-summary element, within the component'
+    );
+    assert.equal(
+      $components.find('img.course-image').attr('src'),
+      'abc',
+      'Course image can be found in a .course-image img element, within the component'
+    );
   });
 
   test('Rendering performance optimizations', function(assert) {
@@ -41,7 +76,7 @@ if (require.has('emberli/components/course-list-item')) {
           url: 'abc'
         }
       }
-    })
+    });
     this.render(hbs`{{course-list-item model=model}}`);
 
     this.set('model.title', 'Other course');
@@ -49,8 +84,26 @@ if (require.has('emberli/components/course-list-item')) {
     this.set('model.image-info.square2x.url', 'http://placehold.it/200x200');
 
     let $components = this.$().children('.course-list-item');
-    assert.equal($components.find('.course-title').text().trim(), 'My Course', 'Course title does not update in response to data changes');
-    assert.equal($components.find('.course-summary').text().trim(), 'A course description', 'Course summary does not update in response to data changes');
-    assert.equal($components.find('img.course-image').attr('src'), 'abc', 'Course image does not update in response to data changes');
+    assert.equal(
+      $components
+        .find('.course-title')
+        .text()
+        .trim(),
+      'My Course',
+      'Course title does not update in response to data changes'
+    );
+    assert.equal(
+      $components
+        .find('.course-summary')
+        .text()
+        .trim(),
+      'A course description',
+      'Course summary does not update in response to data changes'
+    );
+    assert.equal(
+      $components.find('img.course-image').attr('src'),
+      'abc',
+      'Course image does not update in response to data changes'
+    );
   });
 }
