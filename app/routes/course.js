4d21e3744401488959abf0242d580f21c8380a34
Michael North <michael.l.north@gmail.com>
2017-12-19 10:12:22 -0800

    Exercise 6 solution

--- app/routes/course.js
+++ app/routes/course.js
@@ -1,4 +1,9 @@
 import Route from '@ember/routing/route';
 
-export default Route.extend({
-});
+export default class CourseRoute extends Route {
+  model(params) {
+    let { id } = params; // let id = params.id;
+    let allCourses = this.modelFor('application');
+    return allCourses.filter(c => c.title === id)[0];
+  }
+}
