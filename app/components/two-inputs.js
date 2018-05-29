d6e068fdf9fb969cebab6e5d69c983a325bac274
Mike North <michael.l.north@gmail.com>
2018-05-29 11:53:14 -0700

    Ex5 Computed properties

---
+++ app/components/two-inputs.js
@@ -0,0 +1,14 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+export default Component.extend({
+  upperStr: computed('str', {
+    get() {
+      return this.get('str').toUpperCase();
+    },
+    set(_key, upper) {
+      this.set('str', upper.toLowerCase());
+      return upper.toUpperCase();
+    }
+  })
+});
