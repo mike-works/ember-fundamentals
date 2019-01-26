431491c3050d8d186807b562a849399625806c12
Mike North <michael.l.north@gmail.com>
2019-01-25 22:16:57 -0800

    feat: 3.x compatibility

--- app/app.js
+++ app/app.js
@@ -3,6 +3,9 @@ import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
+import Controller from '@ember/controller';
+import { alias } from '@ember/object/computed';
+
 const App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
@@ -10,5 +13,11 @@ const App = Application.extend({
 });
 
 loadInitializers(App, config.modulePrefix);
+
+// Maintain compatibility w/ ember 1.13 -> 3.7.1 and beyond
+// https://www.emberjs.com/deprecations/v2.x/#toc_controller-content-alias
+Controller.reopen({
+  content: alias('model')
+});
 
 export default App;
